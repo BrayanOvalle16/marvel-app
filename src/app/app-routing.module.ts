bb3517fd3dcd53e9a595e23e219d6e200a3d74ee
BrayanOvalle16 <86580913+BrayanOvalle16@users.noreply.github.com>
2022-07-10 21:34:18 -0500

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { ItemComponent } from './landing-page/item/item.component';
 
 const routes: Routes = [
-  {path: "landing-page", loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule)}
+  {path: "landing-page", loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule)},
+  { path: "", redirectTo: 'landing-page', pathMatch: 'full'}
 ];
 
 @NgModule({
